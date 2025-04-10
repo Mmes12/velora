1bbe6c83ff55e748cf85fd68b6060f1ae046ce47
mmes12 <queenmmesoma12@gmail.com>
2025-04-10 12:03:08 +0100

    second commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,49 @@
-import { Main } from "next/document";
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react"
+export default function Home(){
+
+  const[count,setCount] = useState(0)
+
+  useEffect(() => {
+    const getData = async
+  })
+  return(
+    <main className=" flex flex-col gap-20 items-center justify-center px-20 py-20">
+      <p className="text-4xl font-bold ">COUNT</p>
+      <button>INCREMENT</button>
+      <button>DECREMENT</button>
+    </main>
+  )
+}
+/*import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>landing page</main>
+    <main>
+      <div>
+        <p>my image</p>
+        <Image width={300} height={300} src="/download.jpg" alt="image"/>
+      </div>
+    </main>
   );
-}
+}*/
+
+/*Assignment:create a landing page or a home page;
+export default function landingPage(){
+  return(
+    <main>
+      <div className="">
+        <ul className=" flex ">
+          <li>Home</li>
+          <li>About us</li>
+          <li>contact us</li>
+          <input type="button" placeholder="sign-in"></input>
+        </ul>
+        <Image width={300} height={300} src="/pexels-ekateri" alt="image" />
+        </div>
+    </main>
+  );
+}*/
+
+
+
